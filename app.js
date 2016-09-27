d62f1d38bb4da28d65fda7a9ba091400fb46c604
Khaled Garbaya <khaled@contentful.com>
2016-09-27 13:27:20 +0200

    Add brand section, gzip compress and helmet

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ var path = require('path')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-
+var compression = require('compression')
 var products = require('./routes/products')
 var categories = require('./routes/categories')
+var brands = require('./routes/brands')
+
 var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
-
+app.set('view cache', true)
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', products)
 app.use('/products', products)
 app.use('/categories', categories)
+app.use('/brand', brands)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,6 +40,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.use(logger('dev'))
   app.use(function (err, req, res, next) {
     res.status(err.status || 500)
     res.render('error', {
