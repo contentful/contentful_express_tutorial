3d9cbffbfc5aedfb869e4b325174f479aed11c7f
Khaled Garbaya <khaled@contentful.com>
2016-09-27 17:49:45 +0200

    Fix helmet middleware

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var path = require('path')
-// var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
@@ -17,8 +16,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 app.set('view cache', true)
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(helmet) // protect from well known vulnerabilities
+app.use(helmet()) // protect from well known vulnerabilities
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
