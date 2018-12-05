05dabf6e143554e9be44c4cb97799553940e91e8
Cooper Kunz <cooperakunz@gmail.com>
2018-12-04 18:39:19 -0700

    Upgrade dependencies, incl. Jade -> Pug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 app.set('view cache', true)
 app.use(helmet()) // protect from well known vulnerabilities
 app.use(bodyParser.json())
