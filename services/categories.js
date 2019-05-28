c086a309b400d0159320beecfbf5044d7a00ee63
Charlie Chrisman <charlie.chrisman@gmail.com>
2019-05-28 15:08:40 +0200

    update content type ids

--- services/categories.js
+++ services/categories.js
@@ -4,14 +4,14 @@ function getCategory (id, query) {
   // little trick to get an entry with include
   // this way all linked items will be resolved for us
   query = query || {}
-  query['content_type'] = '6XwpTaSiiI2Ak2Ww0oi6qa'
+  query['content_type'] = 'category'
   query['sys.id'] = id
   return client.getEntries(query)
 }
 
 function getCategories (query) {
   query = query || {}
-  query.content_type = '6XwpTaSiiI2Ak2Ww0oi6qa'
+  query.content_type = 'category'
   return client.getEntries(query)
 }
 module.exports = {
